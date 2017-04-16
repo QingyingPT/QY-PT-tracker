34ba361012f331c990e1a7f5c253ebbd718e29fe
zunsthy <zunsthy@gmail.com>
2017-04-16 17:13:39 +0800

    optimize queries

--- Tracker/Task/UpdateTorrent.php
+++ Tracker/Task/UpdateTorrent.php
@@ -1,19 +1,33 @@
 <?php namespace Tracker\Task;
 
 use Tracker\SQL;
+use Tracker\SQLHelper;
 
 class UpdateTorrent extends SQL {
-  public function updatePeers() {
-    $res = $this->sql->query("SELECT torrent, SUM(seeder) as seeders, COUNT(*) as peers FROM tracker_peers GROUP BY torrent")
-      or $this->throwSQLError('SELECT Error');
+  use SQLHelper;
 
-    // TODO: use "INSERT INTO ... DUPLICATE UPDATE ..."
-    while($row = $res->fetch_assoc()) {
-      $this->sql->query("UPDATE torrents SET seeders = '$row[seeders]', leechers = '" .($row['peers'] - $row['seeders']) ."' WHERE id = '$row[torrent]'")
-      or $this->throwSQLError('UPDATE Error');
+  public function updatePeers($difftime) {
+    $dt = esc(date('Y-m-d H:i:s', round(TIMENOW - $difftime / 2)));
+
+    $ret = 0;
+    $this->sql->query("INSERT INTO torrents (id, seeders, leechers)"
+      . " SELECT t.id, t.info_hash, SUM(p.seeder), (COUNT(*) - SUM(p.seeder))"
+      . "   FROM tracker_peers AS p RIGHT JOIN torrents AS t ON t.id = p.torrent"
+      . "   GROUP BY p.torrent"
+      . " ON DUPLICATE KEY"
+      . " UPDATE seeders = VALUES(seeders), leechers = VALUES(leechers), last_update = CURRENT_TIMESTAMP"
+    );
+
+    if ($this->sql->error) {
+      $this->throwSQLError('Can\'t update torrent info');
+      return 0;
     }
 
-    return $this->sql->affected_rows / 2;
+    $this->sql->query("UPDATE torrents SET seeders = 0, leechers = 0, visible = 'no', last_update = CURRENT_TIMESTAMP WHERE last_update < '$dt'");
+    // TODO: check error
+
+    $ret = $this->sql->affected_rows;
+    return $ret;
   }
 }
 
